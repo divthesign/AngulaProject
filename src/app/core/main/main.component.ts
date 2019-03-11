690ace6b7feec63ff049d439fe23bbea26eb6fa4
Daniel <verdudaniel7@gmail.com>
2019-03-11 15:24:04 +0100

    operador 'else'

--- src/app/core/main/main.component.ts
+++ src/app/core/main/main.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-
+  logoAngular : String
   constructor() { }
 
   ngOnInit() {
+    this.logoAngular = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/250px-Angular_full_color_logo.svg.png"
   }
 
 }
